c9fb233b1e404f25ffdfe816455bc8aa99a60f44
dokhala <dokhaclam@gmail.com>
2021-10-15 13:30:03 +0700

    lam google storage 4 form edit

--- resources/views/admin/pages/product/form.blade.php
+++ resources/views/admin/pages/product/form.blade.php
@@ -186,26 +186,51 @@
     </div>
     <div class="panel-body">
        
-      <div class="row" id="preview">
-        <div class="gallery" >
-        </div>
+        <div class="row" id="preview">
+            <div class="gallery" >
+            </div>
          
-      </div>
+        </div>
 
 
-      <input type="file" class="form-control" name="multi-image[]" multiple accept="image/*" id="gallery-photo-add"/>
+        <input type="file" class="form-control" name="multi-image[]" multiple accept="image/*" id="gallery-photo-add"/>
       <!-- hien thi nhieu image -->
-      <div class="row" >
-      @if(isset($image_gallery) && !empty($image_gallery))
-        @foreach($image_gallery as $gallery)
-                <div class="col-lg-3 delete-ele-{{$gallery -> id}}">
-                    <img src="{{ url('uploads/'.$product -> id.'/large/' . $gallery->image) }}" class="photo_preview" />
-                    <a href="#" class="btn btn-danger remove-feature-product"  title="xóa" data-value="{{ $gallery -> id}}"><i class="fa fa-remove"></i></a>
-                </div>
-        @endforeach     
-       @endif       
+        {{-- <div class="row" >
+          @if(isset($image_gallery) && !empty($image_gallery))
+            @foreach($image_gallery as $gallery)
+                    <div class="col-lg-3 delete-ele-{{$gallery -> id}}">
+                        <img src="{{ url('uploads/'.$product -> id.'/large/' . $gallery->image) }}" class="photo_preview" />
+                        <a href="#" class="btn btn-danger remove-feature-product"  title="xóa" data-value="{{ $gallery -> id}}"><i class="fa fa-remove"></i></a>
+                    </div>
+            @endforeach     
+          @endif       
+        </div> --}}
+        <div class="row" >
+          @if(isset($image_gallery) && !empty($image_gallery))
+            @php 
+                //su dung google drive
+                $googleDriveStorage = Storage::disk('small_google_drive');
+            @endphp
+            @foreach($image_gallery as $gallery)
+                    <div class="col-lg-3 delete-ele-{{$gallery -> id}}">
+                        @php 
+                            //get hình ảnh từ google drive
+                            $fileinfo = collect($googleDriveStorage->listContents('/', false))
+                                ->where('type', 'file')
+                                ->where('name', $gallery -> image)
+                                ->first();   
+                        @endphp
+                        @if($fileinfo <> null)
+                        <img src="<?php echo $googleDriveStorage -> url($fileinfo['path']);?>" class="photo_preview" />
+                       
+                       
+                        <a href="#" class="btn btn-danger remove-feature-product"  title="xóa" data-value="{{ $gallery -> id}}"><i class="fa fa-remove"></i></a>
+                         @endif
+                    </div>
+            @endforeach     
+           @endif       
          </div>
-     </div> 
+    </div> 
 
 </div>
       
